9d08d4e421017c94fb814c7130605c66d323eb7f
guoh27 <hong.guo@advantech.com.cn>
2025-06-23 17:05:04 +0800

    Reuse existing attributes DBC for attribute definition tests

--- tests/APITests.cpp
+++ tests/APITests.cpp
@@ -249,7 +249,7 @@ TEST_CASE("API Test: Message", "[]")
 }
 TEST_CASE("API Test: Attribute Definition Access", "[]")
 {
-    std::filesystem::path dbc_path = std::filesystem::path(TEST_FILES_PATH) / "dbc" / "attribute_definition_access.dbc";
+    std::filesystem::path dbc_path = std::filesystem::path(TEST_FILES_PATH) / "dbc" / "attributes.dbc";
     std::ifstream dbc(dbc_path);
     auto net = INetwork::LoadDBCFromIs(dbc);
     REQUIRE(net);
@@ -266,80 +266,64 @@ TEST_CASE("API Test: Attribute Definition Access", "[]")
         return nullptr;
     };
 
-    const IAttributeDefinition* net_def = find_def("NetAttr", IAttributeDefinition::EObjectType::Network);
+    const IAttributeDefinition* net_def = find_def("TheNetworkAttribute", IAttributeDefinition::EObjectType::Network);
     REQUIRE(net_def);
     bool found = false;
     for (const auto& av : net->AttributeValues())
     {
-        if (av.Name() == "NetAttr")
+        if (av.Name() == "TheNetworkAttribute")
         {
             REQUIRE(av.Definition());
             REQUIRE(av.Definition().get() == net_def);
-            REQUIRE(std::get<std::string>(av.Value()) == "net-value");
+            REQUIRE(std::get<int64_t>(av.Value()) == 51);
             found = true;
         }
     }
     REQUIRE(found);
 
     const auto& node = net->Nodes_Get(0);
-    const IAttributeDefinition* node_def = find_def("NodeAttr", IAttributeDefinition::EObjectType::Node);
+    const IAttributeDefinition* node_def = find_def("TheNodeAttribute", IAttributeDefinition::EObjectType::Node);
     REQUIRE(node_def);
     found = false;
     for (const auto& av : node.AttributeValues())
     {
-        if (av.Name() == "NodeAttr")
+        if (av.Name() == "TheNodeAttribute")
         {
             REQUIRE(av.Definition());
             REQUIRE(av.Definition().get() == node_def);
-            REQUIRE(std::get<int64_t>(av.Value()) == 7);
+            REQUIRE(std::get<int64_t>(av.Value()) == 99);
             found = true;
         }
     }
     REQUIRE(found);
 
     const auto& msg = net->Messages_Get(0);
-    const IAttributeDefinition* msg_def = find_def("MsgAttr", IAttributeDefinition::EObjectType::Message);
+    const IAttributeDefinition* msg_def = find_def("TheFloatAttribute", IAttributeDefinition::EObjectType::Message);
     REQUIRE(msg_def);
     found = false;
     for (const auto& av : msg.AttributeValues())
     {
-        if (av.Name() == "MsgAttr")
+        if (av.Name() == "TheFloatAttribute")
         {
             REQUIRE(av.Definition());
             REQUIRE(av.Definition().get() == msg_def);
-            REQUIRE(std::get<int64_t>(av.Value()) == 10);
+            REQUIRE(std::get<double>(av.Value()) == Catch::Approx(58.7));
             found = true;
         }
     }
     REQUIRE(found);
 
     const auto& sig = msg.Signals_Get(0);
-    const IAttributeDefinition* sig_def = find_def("SigAttr", IAttributeDefinition::EObjectType::Signal);
+    const IAttributeDefinition* sig_def = find_def("TheSignalStringAttribute", IAttributeDefinition::EObjectType::Signal);
     REQUIRE(sig_def);
     found = false;
     for (const auto& av : sig.AttributeValues())
     {
-        if (av.Name() == "SigAttr")
+        if (av.Name() == "TheSignalStringAttribute")
         {
             REQUIRE(av.Definition());
             REQUIRE(av.Definition().get() == sig_def);
-            REQUIRE(std::get<double>(av.Value()) == Catch::Approx(5.5));
-            found = true;
-        }
-    }
-    REQUIRE(found);
-
-    const auto& env = net->EnvironmentVariables_Get(0);
-    const IAttributeDefinition* env_def = find_def("EnvAttr", IAttributeDefinition::EObjectType::EnvironmentVariable);
-    REQUIRE(env_def);
-    found = false;
-    for (const auto& av : env.AttributeValues())
-    {
-        if (av.Name() == "EnvAttr")
-        {
-            REQUIRE(av.Definition());
-            REQUIRE(av.Definition().get() == env_def);
-            REQUIRE(std::get<int64_t>(av.Value()) == 1);
+            REQUIRE(std::get<std::string>(av.Value()) == "TestString");
             found = true;
         }
     }
